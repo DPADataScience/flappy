c64a9528c906870e87d931d611c2c88ba4e99afb
keremaslan@outlook.com <kerem.aslan@dpa.nl>
2018-05-13 22:54:46 +0200

    Introduced Agent and Environment to encapsulate the playing agent and environment.

--- src/FlappyBirdController.py
+++ src/FlappyBirdController.py
@@ -2,21 +2,16 @@ from PIL import ImageGrab, Image
 import time
 import numpy as np
 import cv2
-import pandas as pd
-import os
 import subprocess
-import random
-import pyautogui
-import win32gui
 from pywinauto.application import  Application
-from pywinauto.keyboard import SendKeys
-from pywinauto.controls.hwndwrapper import HwndWrapper
 from mss import mss
 from collections import deque
 from src.nn import create_model
-from keras.models import load_model
-from pynput.keyboard import Key, Controller
+
 import matplotlib.pyplot as plt
+from src.objects import Environment, Agent
+
+
 
 def launch_flappy(folder='../FlappyBirdClone/', filename = 'flappy.py', timeout=2):
     """"
@@ -35,18 +30,6 @@ def launch_flappy(folder='../FlappyBirdClone/', filename = 'flappy.py', timeout=
     return p
 
 
-def press_space():
-    """"
-    Presses space if a pywinauto application is active
-
-    :return: void
-    """
-    keyboard = Controller()
-    keyboard.press(Key.space)
-    keyboard.release(Key.space)
-
-
-
 def get_application(title='Flappy Bird'):
     """"
     Connects to a window and returns this as an pywintauto.Application
@@ -59,94 +42,6 @@ def get_application(title='Flappy Bird'):
     return app
 
 
-def kill_app(app):
-    """"
-    Kills a pywintauto.Application
-
-    :return: void
-    """
-    app.kill()
-
-
-def get_window_coordinates(app):
-    """"
-    Returns the coordinates of an app as x_start, y_start, x_end, y_end
-
-    :param app: an instance of pywinauto.Application
-    :return rect.left, rect.top, rect.right, rect.bottom: The coordinates in the for of x_start, y_start, x_end, y_end
-    """
-    window = app.window_()
-    hwnd = window.wrapper_object()
-    rect = hwnd.client_area_rect() #Dit stukje code heeft me echt veels te veel moeite gekost om het te achterhalen
-    return rect.left, rect.top, rect.right, rect.bottom
-
-
-def convert_image_to_array(image):
-    """"
-    Converts an image to a numpy array
-
-    :param image: an instane of PIL.Image
-    :return np.array: The image as numpy array
-    """
-    return np.array(image)
-
-
-def convert_array_to_image(arr):
-    return Image.fromarray(arr)
-
-
-def process_image(image):
-    """"
-    Processes an image by converting it to a numpy array, scaling without losing detail and selecting only edges that matter.
-
-    :param image: an instance of mss.screenshot.Screenshot
-    :return np.array: The processed image as numpy array (black/white and scaled)
-    """
-
-    arr = np.array(image)
-    arr = np.delete(arr, np.s_[::2], 0)
-    arr = np.delete(arr, np.s_[::2], 1)
-    arr = np.sum(arr, axis=2)
-    arr = (np.multiply(200 < arr, arr < 227) * 255).astype('uint8')
-    return arr
-
-
-def grab_frame(coordinates):
-    """"
-    grabs screen within coordinates, and apply process_image
-
-    :param coordinates: dict the coordinates of the screen
-    :return image: numpy.array representation of processed images
-    """
-    sct = mss()
-    screen_image = sct.grab(coordinates)
-    processed_image = process_image(screen_image)
-    return processed_image
-
-
-def calculate_reward(state):
-    """"
-    calculate the reward of the state. Flappy is dead when the screen has stopped moving, so when two consecutive frames
-    are equal. A point is scored when an obstacle is above flappy, and before it wasn't. An object is above Flappy when
-    there are two white pixels in the first 50 pixels on the first row.
-
-    :param state: four consecutive processed frames
-    :return reward: int representing the reward if a point is scored or if flappy has died.
-    """
-    if np.sum(state[30:32, :, 3]) == 8415:
-        print("in menu")
-        return -100
-    elif np.sum(state[:, 0:50, 3]) / 255 == 50:
-        print("buiten scherm")
-        return -100
-    elif np.sum((state[:,:,3] - state[:,:,2])) == 0 and np.sum((state[:,:,2] - state[:,:,1])) == 0:
-        print("flappy is dood")
-        return -1000
-    elif sum(state[0,:50,3]) == 510 and sum(state[0,:50,2]) == 510 and sum(state[0,:50,1]) != 510 and sum(state[0,:50,0]) != 510:
-        print("punt gescoord!")
-        return 1000
-    else:
-        return 0
 
 def main():
 
@@ -158,78 +53,19 @@ def main():
     # Play game and grab screen
     launch_flappy()
     app = get_application()
-    x_start, y_start, x_end, y_end = get_window_coordinates(app)
-    coordinates = {'top': y_start,
-                   'left': x_start,
-                   'height': y_end - y_start - 106,
-                   'width': x_end - x_start
-                   }
 
-    # Set time parameters
-    FPS = 30
-    stacked_frames = 4
-    GAMMA = 0.9
-    t_end = time.time() + 60
-    framerate = 1/FPS
+    environment = Environment(app, FPS=30, stacked_frames=4)
 
-    # Prepare states, action  and model
-    stack = deque(maxlen=stacked_frames)
-    for i in range(stacked_frames):
-        frame = grab_frame(coordinates)
-        stack.extend([frame])
-        time.sleep(framerate)
-    current_state = np.dstack(stack)
+    agent = Agent(environment)
+    agent.train(5)
 
-    model = create_model()
-    #model = load_model('flappy_model.h5')
+    environment.close()
+    # # Updating the processed image
+    # cv2.imshow('image', current_state[:, :, 3])
+    # cv2.waitKey(1)
 
-    replay_memory = []
-
-    action = 0
-    iteraties = 0
-    plt.show()
+    # cv2.destroyAllWindows()
 
 
-
-    t_end = time.time() + 20
-    while time.time() < t_end:
-        start = time.time()
-
-        # action
-        prediction = model.predict(x=current_state[np.newaxis, ...])[0]
-
-        if prediction[1] > prediction[0]:
-            action = 1
-            press_space()
-        else:
-            action = 0
-
-        # make tuple (previous_states, action, reward, current_state)
-        frame = grab_frame(coordinates)
-        stack.extend([frame])
-        previous_state = current_state
-        current_state = np.dstack(stack)
-        reward = calculate_reward(previous_state)
-        
-        # updating the model
-        y = prediction
-        y[action] = reward + GAMMA * np.max(model.predict(x=current_state[np.newaxis, ...]))
-        model.fit(x=previous_state[np.newaxis, ...], y=np.array([y]), verbose=0)
-
-        # Wait for next frame
-        time_to_process = time.time()-start
-        print('time to process', time_to_process)
-        time.sleep(max(0.0, framerate - time_to_process))
-        iteraties = iteraties + 1
-
-        # Updating the processed image
-        cv2.imshow('image', current_state[:, :, 3])
-        cv2.waitKey(1)
-
-
-    kill_app(app)
-    model.save('flappy_model.h5')
-    print(iteraties)
-    cv2.destroyAllWindows()
 if __name__ == "__main__":
     main()
---
+++ src/objects.py
@@ -0,0 +1,244 @@
+import gym
+from mss import mss
+import numpy as np
+from PIL import Image
+from pynput.keyboard import Key, Controller
+from collections import deque
+import time
+from src import nn
+
+class Agent:
+    def __init__(self, environment,  disount_rate=0.9, epsilon=1.0, max_epsilon=1.0, min_epsilon=0.01, decay_rate=0.01):
+        """"
+        Creates an instance of Agent.
+
+        :param environment: Environment the agent is created for
+        :param disount_rate: float The gamma value of Reinnfocement learning - i.e. the cumulative discoutned reward value
+        :param epsilon: float A value between 0 and 1 that is used in the training stages of the agent. The higher the value
+        the likelier the agent will take random actions
+        :param max_epsilon: float The max value of epsilon
+        :param min_epsilon: float the min value of epsilon
+        :param decay_rate: float The decay rate to be used by the agent. The higher the rate the quicker the agent will
+        reduce the value of epsilon
+        """
+        self.env = environment
+        self.gamma = disount_rate
+        self.epsilon = epsilon
+        self.max_epsilon = max_epsilon
+        self.min_epsilon = min_epsilon
+        self.decay_rate = decay_rate
+        self.model = nn.create_model() #TODO create network:  _create_network()
+
+
+
+    def _create_network(self):
+        raise NotImplementedError("yet to be implemented")
+
+    def train(self, total_episodes):
+        """"
+        Trains the agent to play the game assigned to this agent
+        """
+        # raise NotImplementedError
+        rewards = []
+        for episode in range(total_episodes):
+            state = self.env.reset()
+            total_rewards = 0
+            while True:
+                # here we implement the exploration possibility in the beginning we want to take random actions
+                # so the agent can explore the environment, later once it has learned about the environment it can
+                # start to take actions it knows
+                predicted = self.model.predict(state)
+                action = np.argmax(predicted)
+                if np.random.uniform(low=0, high=1) < self.epsilon:
+                    action = np.random.choice(self.env.action_space)
+
+                # take the action and observe the outcome state and reward
+                next_state, reward, done, info = self.env.step(action)
+
+                # retrain the neural net
+                predicted_next_state = self.model.predict(next_state)
+                Qmax = np.max(predicted_next_state)
+                y = predicted
+
+                print("y", y)
+                y[0][action] = reward + self.gamma * np.max(self.model.predict(x=state))
+                self.model.fit(x=state, y=y, verbose=0)
+
+                state = next_state
+                total_rewards += reward
+
+                if done:
+                    break
+
+                # adjust epsilon by an exponential function with lower bound min_epsilon
+                self.epsilon = self.min_epsilon + (self.max_epsilon - self.min_epsilon) * np.exp(-self.decay_rate * episode)
+                rewards.append(total_rewards)
+
+        print("Score over time: " + str(sum(rewards) / total_episodes))
+
+
+    def play(self):
+            raise NotImplementedError
+
+
+class Environment:
+    def __init__(self, app, FPS=30, stacked_frames=4):
+        """"
+        Creates an instance of Environment
+        """
+        self.app = app
+        self.fps = FPS
+        self.stacked_frames = stacked_frames
+        self.framerate = 1/FPS
+        self.coordinates = self._get_window_coordinates(app)
+        self.action_space = (0, 1) #the possibe actions in this environment
+
+
+    def _get_window_coordinates(self, app):
+        """"
+        Returns the coordinates of an app as x_start, y_start, x_end, y_end
+
+        :param app: an instance of pywinauto.Application
+        :return coordinates: dict The coordinates in the for of x_start, y_start, x_end, y_end
+        """
+        window = app.window_()
+        hwnd = window.wrapper_object()
+        rect = hwnd.client_area_rect() #Dit stukje code heeft me echt veels te veel moeite gekost om het te achterhalen
+        # return rect.left, rect.top, rect.right, rect.bottom
+
+        coordinates = {'top': rect.top,
+                       'left': rect.left,
+                       'height': rect.bottom - rect.top - 106,
+                       'width': rect.right - rect.left
+                       }
+
+        return coordinates
+
+    def _grab_frame(self, coordinates):
+        """"
+        grabs screen within coordinates, and apply process_image
+
+        :param coordinates: dict the coordinates of the screen
+        :return image: numpy.array representation of processed images
+        """
+        sct = mss()
+        screen_image = sct.grab(coordinates)
+        processed_image = self._process_image(screen_image)
+        return processed_image
+
+    def _process_image(self, image):
+        """"
+        Processes an image by converting it to a numpy array, scaling without losing detail and selecting only edges that matter.
+
+        :param image: an instance of mss.screenshot.Screenshot
+        :return np.array: The processed image as numpy array (black/white and scaled)
+        """
+
+        arr = np.array(image)
+        arr = np.delete(arr, np.s_[::2], 0)
+        arr = np.delete(arr, np.s_[::2], 1)
+        arr = np.sum(arr, axis=2)
+        arr = (np.multiply(200 < arr, arr < 227) * 255).astype('uint8')
+        return arr
+
+    def _convert_image_to_array(self, image):
+        """"
+        Converts an image to a numpy array
+
+        :param image: an instane of PIL.Image
+        :return np.array: The image as numpy array
+        """
+        return np.array(image)
+
+    def convert_array_to_image(self, arr):
+        return Image.fromarray(arr)
+
+    def _calculate_reward(self, state):
+        """"
+        calculate the reward of the state. Flappy is dead when the screen has stopped moving, so when two consecutive frames
+        are equal. A point is scored when an obstacle is above flappy, and before it wasn't. An object is above Flappy when
+        there are two white pixels in the first 50 pixels on the first row.
+
+        :param state: four consecutive processed frames
+        :return reward: int representing the reward if a point is scored or if flappy has died.
+        """
+        if np.sum(state[30:32, :, 3]) == 8415:
+            print("in menu")
+            return -100
+        elif np.sum(state[:, 0:50, 3]) / 255 == 50:
+            print("buiten scherm")
+            return -100
+        elif np.sum((state[:,:,3] - state[:,:,2])) == 0 and np.sum((state[:,:,2] - state[:,:,1])) == 0:
+            print("flappy is dood")
+            return -1000
+        elif sum(state[0,:50,3]) == 510 and sum(state[0,:50,2]) == 510 and sum(state[0,:50,1]) != 510 and sum(state[0,:50,0]) != 510:
+            print("punt gescoord!")
+            return 1000
+        else:
+            return 0
+
+    def _press_space(self):
+        """"
+        Presses space if a pywinauto application is active
+        """
+        keyboard = Controller()
+        keyboard.press(Key.space)
+        keyboard.release(Key.space)
+
+    def reset(self):
+        """"
+        Resets the state of the environment and returns an initial observation.
+
+        :return observation: np.array Returns an observation of the environmet after starting the game. The dimensions
+        of the array are #TODO(, self.stacked_Frames)
+        """
+        self._press_space()
+        observation = self._stream()
+        return observation
+
+    def _stream(self):
+        stack = deque(maxlen=self.stacked_frames)
+        for i in range(self.stacked_frames):
+            frame = self._grab_frame(self.coordinates)
+            stack.extend([frame])
+            time.sleep(self.framerate)
+
+        state = np.dstack(stack)
+        return state[np.newaxis, ...]
+
+    def step(self, action):
+        """"
+        Run one timestep of the environment's dynamics. When end of
+        episode is reached, you are responsible for calling `reset()`
+        to reset this environment's state.
+        Accepts an action and returns a tuple (observation, reward, done, info).
+
+        :param action: int 0 for do nothing 1 for press space
+        :return observation: np.array
+        :return reward: float  amount of reward returned after previous action
+        :return done: boolean whether the episode has ended, in which case further step() calls will return undefined results
+        :return info: ontains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+
+        """
+        if action == 1:
+            self._press_space()
+
+        observation = self._stream()
+        reward = self._calculate_reward(observation)
+        done = False
+        info = {}
+        if reward == -1000:
+            done = True
+
+        return observation, reward, done, info
+
+    def close(self):
+        self._kill_app()
+
+    def _kill_app(self):
+        """"
+        Kills a pywintauto.Application
+
+        :return: void
+        """
+        self.app.kill()
